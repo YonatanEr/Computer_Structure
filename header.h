7c8c215c9a3ef9961299a405c8626cb25b223bda
YonatanEr <76260092+YonatanEr@users.noreply.github.com>
2022-12-05 17:33:26 +0200

    header.h
    header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "utils/file_utils.c"
+#include "utils/slice_utils.c"
+#include "utils/split_utils.c"
+
+#include "structs/char_array.c"
+#include "structs/char_matrix.c"
+#include "structs/int_array.c"
